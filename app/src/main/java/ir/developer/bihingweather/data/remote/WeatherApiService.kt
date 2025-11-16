41bdaea771b893bf3caee79f9e4af08fd2b350b0
SaraSaber <saraasaber77@gmail.com>
2025-11-16 17:06:05 +0330

    feat(apiService in data)

---
+++ app/src/main/java/ir/developer/bihingweather/data/remote/WeatherApiService.kt
@@ -0,0 +1,15 @@
+package ir.developer.bihingweather.data.remote
+
+import ir.developer.bihingweather.domain.model.WeatherResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+    @GET("forecast")
+    suspend fun getWeatherForecast(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): WeatherResponse
+}
