617eb2a55b27ab35126a99ee4f761052057e10d0
SaraSaber <saraasaber77@gmail.com>
2025-11-16 14:08:45 +0330

    feat(packaging)

--- app/src/main/java/ir/developer/bihingweather/presentation/ui/theme/Type.kt
+++ app/src/main/java/ir/developer/bihingweather/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package ir.developer.bihingweather.ui.theme
+package ir.developer.bihingweather.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
