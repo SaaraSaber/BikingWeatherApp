db592120a40e38e029ff05778f72b9539e08739e
SaraSaber <saraasaber77@gmail.com>
2025-11-16 17:04:02 +0330

    feat(module)

---
+++ app/src/main/java/ir/developer/bihingweather/di/AppModule.kt
@@ -0,0 +1,38 @@
+package ir.developer.bihingweather.di
+
+import ir.developer.bihingweather.data.remote.Config
+import ir.developer.bihingweather.data.remote.WeatherApiService
+import ir.developer.bihingweather.data.repository.WeatherRepositoryImpl
+import ir.developer.bihingweather.domain.repository.WeatherRepository
+import ir.developer.bihingweather.domain.usecase.CalculateBikingRidingScoreUseCase
+import ir.developer.bihingweather.domain.usecase.GetWeatherForecastUseCase
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val appModule = module {
+    single {
+        Retrofit.Builder().baseUrl(Config.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    single {
+        get<Retrofit>().create(WeatherApiService::class.java)
+    }
+
+    //Repository
+    single<WeatherRepository> {
+        WeatherRepositoryImpl(get())
+    }
+
+    //usecase
+    single {
+        GetWeatherForecastUseCase(get())
+    }
+    single {
+        CalculateBikingRidingScoreUseCase()
+    }
+
+    //viewmodel
+
+}
