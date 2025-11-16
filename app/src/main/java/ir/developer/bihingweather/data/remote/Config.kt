b0514f775e939161d729b650b1a08c8380f78292
SaraSaber <saraasaber77@gmail.com>
2025-11-16 17:04:11 +0330

    feat(config)

---
+++ app/src/main/java/ir/developer/bihingweather/data/remote/Config.kt
@@ -0,0 +1,7 @@
+package ir.developer.bihingweather.data.remote
+
+object Config {
+    const val OPEN_WEATHER_API_KEY = "3e03d2e622c377632dbed0ed8b68b4b8"
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    const val WEATHER_ICON_BASE_URL = "https://openweathermap.org/img/wn/"
+}
