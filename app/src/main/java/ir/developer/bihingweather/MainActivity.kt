81533aa565537f0a649e227dc78d5216be792cef
SaraSaber <saraasaber77@gmail.com>
2025-11-22 14:49:06 +0330

    feat(WeatherScreen)

--- app/src/main/java/ir/developer/bihingweather/MainActivity.kt
+++ app/src/main/java/ir/developer/bihingweather/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import ir.developer.bihingweather.presentation.screens.WeatherScreen
 import ir.developer.bihingweather.presentation.ui.theme.BihingWeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             BihingWeatherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    WeatherScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BihingWeatherTheme {
-        Greeting("Android")
     }
 }
